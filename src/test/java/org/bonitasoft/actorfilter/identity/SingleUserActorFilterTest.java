5737de9b951acf2f357ea86043e1f363e8088cc4
Romain Bioteau <romain.bioteau@bonitasoft.com>
2023-11-27 16:11:48 +0100

    chore(build): use java 17 (#2)
    Closes BPM-23

--- src/test/java/org/bonitasoft/actorfilter/identity/SingleUserActorFilterTest.java
+++ src/test/java/org/bonitasoft/actorfilter/identity/SingleUserActorFilterTest.java
@@ -1,24 +1,21 @@
 package org.bonitasoft.actorfilter.identity;
 
-import org.bonitasoft.engine.connector.ConnectorValidationException;
-import org.bonitasoft.engine.filter.UserFilterException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.bonitasoft.actorfilter.identity.SingleUserActorFilter.USER_ID;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.bonitasoft.actorfilter.identity.SingleUserActorFilter.USER_ID;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+import org.bonitasoft.engine.filter.UserFilterException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 class SingleUserActorFilterTest {
 
     @InjectMocks
